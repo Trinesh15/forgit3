8b614cbb3f13fef22bbf57f5e7db963732dcd2df
trinesh <trinesh@biosoftltd.com>
2026-02-18 10:30:51 +0530

    Commiting the project day 3

---
+++ src/Package/RevString.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class RevString {
+	public static void main(String[]args) {
+		String a = "Trinesh";
+		String rev;
+		for(int i=a.length(); i>=0;i--) {
+			
+		}
+	}
+
+}
